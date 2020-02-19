cc997b81bfb3e27b8d74deb3b774cd3bf004c943
Dushigub <gonohono1703@gmail.com>
2020-02-19 03:04:55 +0300

    one

---
+++ index.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, maximum-scale=1">
+
+	<title>Fuck Society</title>
+	<link rel="icon" href="favicon.png" type="image/png">
+	<link rel="shortcut icon" href="favicon.ico" type="img/x-icon">
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+	<link href="css/responsive.css" rel="stylesheet" type="text/css">
+	<link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+	<link href="css/animate.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="js/jquery.1.8.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/jquery.isotope.js"></script>
+	<script type="text/javascript" src="js/wow.js"></script>
+	<script type="text/javascript" src="js/classie.js"></script>
+	<script type="text/javascript" src="js/magnific-popup.js"></script>
+	<script src="contactform/contactform.js"></script>
+</head>
+
+<body>
+	<header class="header" id="header">
+		<div class="container">
+			<h1 class="animated fadeInDown delay-07s">Welcome to Fuck Society</h1>
+			<ul class="we-create animated fadeInUp delay-1s">
+				<li>Наблюдайте как мы изменим мир IT</li>
+			</ul>
+			<a class="link animated fadeInUp delay-1s servicelink" href="#service">Начать</a>
+		</div>
+	</header>
+	<nav class="main-nav-outer" id="test">
+		<div class="container">
+			<ul class="main-nav">
+				<li><a href="#header">Home</a></li>
+				<li><a href="#service">Commands</a></li>
+				<li><a href="#team">Team</a></li>
+			</ul>
+			<a class="res-nav_click" href="#"><i class="fa fa-bars"></i></a>
+		</div>
+	</nav>
+	<section class="main-section" id="service">
+		<div class="container">
+			<h2>Commands</h2>
+			<h6>Топ команды бота.</h6>
+			<div class="row">
+				<div class="col-lg-4 col-sm-6 wow fadeInLeft delay-05s">
+					<div class="service-list">
+						<div class="service-list-col1">
+						</div>
+						<div class="service-list-col2">
+						  <a class="list-group-item"><i class="fa fa-user-secret"></i>&nbsp; /search</a>
+							<h5>Search делится на 3 команды</h5>
+<h5>/USER - Пробив никнейма по всем соц. сетям</h5>
+<h5>/IP_INFO даёт полную информация об IP</h5>
+<h5>/PHONE - Парсит сайты для поиска информации</h5>
+						</div>
+					</div>
+					<div class="service-list">
+						<div class="service-list-col1">
+						</div>
+						<div class="service-list-col2">
+							<a class="list-group-item"><i class="fa fa-comments-o"></i>&nbsp; /anonymous</a>
+							<h5>Анонимный чат с рандомным человеком. Пользователи не видят друг друга, общаются через бота</h5>
+						</div>
+					</div>
+					<div class="service-list">
+						<div class="service-list-col1">
+						</div>
+						<div class="service-list-col2">
+            <a class="list-group-item"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; /TRAP</a>
+							<h5>Trap делится на 2 команды</h5>
+							<h5>/URLS - Показывает куда ведёт URL</h5>
+							<h5>/URL_TRAP - создаёт ссылку для пользователя</h5>
+						</div>
+					</div>
+					<div class="service-list">
+						<div class="service-list-col1">
+						</div>
+						<div class="service-list-col2">
+              <a class="list-group-item"><i class="fa fa-shield"></i>&nbsp; /newname</a>
+							<h5>Придумает за вас новые данные</h5>
+							<h5>Имя, фамилию, старну, адрес, индекс адреса, номер старны</h5>
+						</div>
+					</div>
+					<div class="service-list">
+						<div class="service-list-col1">
+						</div>
+						<div class="service-list-col2">
+							<a class="list-group-item"><i class="fa fa-pie-chart"></i>&nbsp; /statistic</a>
+							<h5>Ведёт статистику группы</h5>
+						</div>
+					</div>
+					<div class="service-list">
+						<div class="service-list-col1">
+						</div>
+						<div class="service-list-col2">
+							<a class="list-group-item"><i class="fa fa-refresh"></i>&nbsp; /replay</a>
+							<h5>Переводит сообщение в голосовое сообщение</h6>
+						</div>
+					</div>
+					<div class="service-list">
+						<div class="service-list-col1">
+						</div>
+						<div class="service-list-col2">
+							<a class="list-group-item"><i class="fa fa-cloud"></i>&nbsp; /weather</a>
+							<h6>Говорит погоду в разных концах света, если не сказал то пишите на EN</h6>
+						</div>
+					</div>
+				</div>
+				<figure class="col-lg-8 col-sm-6  text-right wow fadeInUp delay-02s">
+					<img src="img/dialog.png" alt="">
+				</figure>
+			</div>
+		</div>
+	</section>
+	<section class="main-section alabaster">
+		<div class="container">
+			<div class="row">
+				<figure class="col-lg-5 col-sm-4 wow fadeInLeft">
+					  <img src="img/ai.jpg" alt="">
+				</figure>
+				<div class="col-lg-7 col-sm-8 featured-work">
+					<h2>Искусственный интеллект</h2>
+					<div class="featured-box">
+						<div class="featured-box-col1 wow fadeInRight delay-02s">
+
+						</div>
+						<div class="featured-box-col2 wow fadeInRight delay-02s">
+							<h3>магия развития</h3>
+							<h5>Я не могу сказать точно как работате алгоритм, но у него есть NLP фильтр который выделяет главную мысль для бота, запоминает 44% чтобы вести диалог.</h5>
+						</div>
+					</div>
+					<div class="featured-box">
+						<div class="featured-box-col1 wow fadeInRight delay-04s">
+							<i class="fa fa-book"></i>
+						</div>
+						<div class="featured-box-col2 wow fadeInRight delay-04s">
+							<h3>Бот знает 200-тысяч слов +</h3>
+							<h7>В чатах специально не всегда отвечает, шанс что бот вам ответит 50/50. Хм... Это достаточно много... Если он много говорит пишите нам в ТГ через бота, для обратной связи - /idea</h7>
+						</div>
+					</div>
+					<div class="featured-box">
+						<div class="featured-box-col1 wow fadeInRight delay-06s">
+							<i class="fa fa-language"></i>
+						</div>
+						<div class="featured-box-col2 wow fadeInRight delay-06s">
+							<h3>NLP</h3>
+							<h7>Мощная нейронная сеть в связке с NLP. Работает только на русском.</h7>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="main-section team" id="team">
+		<div class="container">
+			<h2>team</h2>
+			<div class="team-leader-block clearfix">
+				<div class="team-leader-box">
+					<div class="team-leader wow fadeInDown delay-03s">
+						<center><img src="img/anon.jpg" alt=""></center>
+					</div>
+					<h3 class="wow fadeInDown delay-03s">unknown</h3>
+					<span class="wow fadeInDown delay-03s">Директор компании</span>
+					<h5 class="wow fadeInDown delay-03s">Основатель компании Fuck Society, также главный инженер-разработчик.</h5>
+					<h5 class="wow fadeInDown delay-03s">Направление: кибербезопасность, data-science, NLP.</h5>
+				</div>
+				<div class="team-leader-box">
+					<div class="team-leader  wow fadeInDown delay-06s">
+						<center><img src="img/anon.jpg" alt=""></center>
+					</div>
+					<h3 class="wow fadeInDown delay-06s">Dima</h3>
+					<span class="wow fadeInDown delay-06s">Заместитель директора</span>
+					<h5 class="wow fadeInDown delay-06s">Инженер-программист, активно помогает компании.</h5>
+					<h5 class="wow fadeInDown delay-06s">Направление: data-science, db</h5>
+				</div>
+				<div class="team-leader-box">
+					<div class="team-leader wow fadeInDown delay-09s">
+						<center><img src="img/anon.jpg" alt=""></center>
+					</div>
+					<h3 class="wow fadeInDown delay-09s">Yaroslav</h3>
+					<span class="wow fadeInDown delay-09s">Web разработчик</span>
+					<h5 class="wow fadeInDown delay-09s">Инженер-программист, PR-менеджер, вкладывается в web и все что с ним связано.</h5>
+					<h5 class="wow fadeInDown delay-09s">Направление: NLP, пиар.</h5>
+				</div>
+			</div>
+		</div>
+	</section>
+			<h2>I like you</h2>
+	<script type="text/javascript">
+		$(document).ready(function(e) {
+
+			$('#test').scrollToFixed();
+			$('.res-nav_click').click(function() {
+				$('.main-nav').slideToggle();
+				return false
+
+			});
+
+      $('.Portfolio-box').magnificPopup({
+        delegate: 'a',
+        type: 'image'
+      });
+
+		});
+	</script>
+
+	<script>
+		wow = new WOW({
+			animateClass: 'animated',
+			offset: 100
+		});
+		wow.init();
+	</script>
+
+
+	<script type="text/javascript">
+		$(window).load(function() {
+			$('.main-nav li a, .servicelink').bind('click', function(event) {
+				var $anchor = $(this);
+				$('html, body').stop().animate({
+					scrollTop: $($anchor.attr('href')).offset().top - 102
+				}, 1500, 'easeInOutExpo');
+				if ($(window).width() < 768) {
+					$('.main-nav').hide();
+				}
+				event.preventDefault();
+			});
+		})
+	</script>
+	<script type="text/javascript">
+		$(window).load(function() {
+			var $container = $('.portfolioContainer'),
+				$body = $('body'),
+				colW = 375,
+				columns = null;
+			$container.isotope({
+				resizable: true,
+				masonry: {
+					columnWidth: colW
+				}
+			});
+
+			$(window).smartresize(function() {
+				var currentColumns = Math.floor(($body.width() - 30) / colW);
+				if (currentColumns !== columns) {
+					columns = currentColumns;
+					$container.width(columns * colW)
+						.isotope('reLayout');
+				}
+
+			}).smartresize();
+			$('.portfolioFilter a').click(function() {
+				$('.portfolioFilter .current').removeClass('current');
+				$(this).addClass('current');
+
+				var selector = $(this).attr('data-filter');
+				$container.isotope({
+
+					filter: selector,
+				});
+				return false;
+			});
+
+		});
+	</script>
+</body>
+</html>
